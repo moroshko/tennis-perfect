387c1be6b5926d4480b335c172044a6a6e4ece12
Misha Moroshko <michael.moroshko@gmail.com>
2021-06-05 23:06:23 +1000

    Add Olga's picture

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import styles from "./Footer.module.css";
 
 export default function Footer() {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <div className={styles.content}>
         {`Tennis Perfect © ${new Date().getFullYear()}`}{" "}
       </div>
