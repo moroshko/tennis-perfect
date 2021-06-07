38cc8c62312efe72aeb013e41211fa313cff5677
Misha Moroshko <michael.moroshko@gmail.com>
2021-06-07 14:21:13 +1000

    Add tracking to Contact Us

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,3 +1,4 @@
+import { trackEvent } from "../components/Splitbee";
 import PhoneIcon from "../components/icons/Phone";
 import EmailIcon from "../components/icons/Email";
 import styles from "../styles/Contact.module.css";
@@ -8,7 +9,13 @@ function ContactUsPage() {
       <h1>Contact Us</h1>
       <p>We’d love to tell you more about our programs!</p>
       <div className={styles.contactMethods}>
-        <a className={styles.contactLink} href="tel:+61423714373">
+        <a
+          className={styles.contactLink}
+          href="tel:+61423714373"
+          onClick={() => {
+            trackEvent("Contact Us phone number clicked");
+          }}
+        >
           <PhoneIcon />
           <span className={styles.contactLinkContent}>0423 714 373</span>
         </a>
@@ -17,6 +24,9 @@ function ContactUsPage() {
           href="mailto:info@tennisperfect.com.au"
           target="_blank"
           rel="noreferrer"
+          onClick={() => {
+            trackEvent("Contact Us email clicked");
+          }}
         >
           <EmailIcon />
           <span className={styles.contactLinkContent}>
